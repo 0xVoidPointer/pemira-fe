6a4a06e836a8ceb177e0a3bf6de946394e4f226e
Dimas Avrian Fachriza <dimasavrian346@gmail.com>
2026-05-21 20:11:49 +0700

    fix: delete google account option for admin

--- src/features/auth/components/login-form.tsx
+++ src/features/auth/components/login-form.tsx
@@ -92,17 +92,21 @@ export function LoginForm() {
         </div>
       </form>
 
-      <div className="relative w-full my-8">
-        <div className="absolute inset-0 flex items-center">
-          <Separator />
-        </div>
-        <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-card px-2 text-muted-foreground">
-            Atau Masuk Dengan
-          </span>
-        </div>
-      </div>
-      <GoogleLoginButton />
+      {loginAs === "mahasiswa" ? (
+        <>
+          <div className="relative w-full my-8">
+            <div className="absolute inset-0 flex items-center">
+              <Separator />
+            </div>
+            <div className="relative flex justify-center text-xs uppercase">
+              <span className="bg-card px-2 text-muted-foreground">
+                Atau Masuk Dengan
+              </span>
+            </div>
+          </div>
+          <GoogleLoginButton />
+        </>
+      ) : null}
     </section>
   );
 }
