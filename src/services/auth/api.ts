3d517efd834917dc31a55e70ab78283d07326403
Dimas Avrian Fachriza <dimasavrian346@gmail.com>
2026-05-31 14:49:29 +0700

    refactor(services/auth): use generated schemas and remove redundant schema file

--- src/services/auth/api.ts
+++ src/services/auth/api.ts
@@ -1,18 +1,18 @@
 import type z from "zod";
-import { LoginRequestStudentSchema } from "./schema";
 import { http } from "#/lib/http";
+import { zLoginRequestStudent } from "../_generated/schema";
 
-export type LoginRequestStudent = z.infer<typeof LoginRequestStudentSchema>;
+export type LoginRequestStudent = z.infer<typeof zLoginRequestStudent>;
 
-export async function studentLogin(input: LoginRequestStudent) {
+export async function studentLogin(input: LoginRequestStudent): Promise<void> {
   await http.post("student/auth", { json: input });
 }
 
-export async function verifySession(signal?: AbortSignal) {
+export async function verifySession(signal?: AbortSignal): Promise<true> {
   await http.get("student/auth/verify", { signal });
   return true;
 }
 
-export async function logout() {
+export async function logout(): Promise<void> {
   await http.post("student/auth/logout");
 }
--- src/services/auth/schema.ts
+++
@@ -1,6 +0,0 @@
-import z from "zod";
-
-export const LoginRequestStudentSchema = z.object({
-  identifier: z.string().max(255),
-  password: z.string(),
-});
